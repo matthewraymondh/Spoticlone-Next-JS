1654f95b9885d68f23de61734d283f4393325b01
matthewraymondh <raymondhartono76@gmail.com>
2021-12-20 13:11:48 +0700

    spoticlone

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "56IUha6BwV2wIC5Yz6TsD9",
+});
